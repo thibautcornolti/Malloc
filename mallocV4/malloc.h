d91b8ab5824d237e208690c4d29a16fbd16709df
Bastien <bastien.rinck@epitech.eu>
2018-02-07 13:49:14 +0100

    Major fix on malloc realloc calloc and free

---
+++ mallocV4/malloc.h
@@ -0,0 +1,37 @@
+//
+// Created by rectoria on 07/02/18.
+//
+
+#ifndef MALLOC_H
+#define MALLOC_H
+
+#include <unistd.h>
+
+extern struct metadata_s *allocated;
+
+typedef struct metadata_s {
+	size_t size;
+	struct metadata_s *next;
+	struct metadata_s *prev;
+	struct metadata_s *last;
+	char occupied;
+} metadata_t;
+
+void	*malloc(size_t);
+void	*realloc(void *, size_t);
+void	*calloc(size_t, size_t);
+void	free(void *);
+void	lock_thread(int);
+void	unlock_thread(int);
+void	show_alloc_mem(void);
+
+#ifndef __X86_64__
+	#define ALIGNMENT (16)
+#else
+	#define ALIGNMENT (8)
+#endif
+
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) &~ (ALIGNMENT - 1))
+#define HEADER (sizeof(struct metadata_s))
+
+#endif
